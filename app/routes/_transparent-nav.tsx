74f353c902e600eb9f37a14727aee9c60c6430d1
B1gdawg0 <bkaosas@gmail.com>
2025-02-10 00:24:02 +0700

    add: sidebar and route logic

--- app/routes/_transparent-nav.tsx
+++ app/routes/_transparent-nav.tsx
@@ -1,14 +1,22 @@
 import { Outlet } from "@remix-run/react";
 import { AlignJustify } from "lucide-react";
 import { Link } from "@remix-run/react";
+import { useState, useEffect } from "react";
+import SidebarMenu from "~/components/sidebar-menu";
 
 function TransparentNav() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => setIsMenuOpen((prev) => !prev);
+
     return (
         <div className="relative">
-            <Outlet></Outlet>
-            <nav className="fixed top-0 left-0 w-full bg-transparent flex flex-row justify-between items-center px-2 h-[10.4vh] z-50">
+            <Outlet />
+            <nav
+                className={`fixed top-0 left-0 w-full bg-transparent flex flex-row justify-between items-center px-2 h-[10.4vh] z-30 transition-transform duration-300`}
+            >
                 <div className="text-white">
-                    <AlignJustify width={20} height={20} />
+                    <AlignJustify width={20} height={20} onClick={toggleMenu} />
                 </div>
 
                 <div className="text-white text-2xl font-bold">
@@ -17,6 +25,8 @@ function TransparentNav() {
 
                 <div className="rounded-full bg-zinc-600 w-[47px] h-[47px]"></div>
             </nav>
+
+            {isMenuOpen && <SidebarMenu onClose={toggleMenu} />}
         </div>
     );
 }
