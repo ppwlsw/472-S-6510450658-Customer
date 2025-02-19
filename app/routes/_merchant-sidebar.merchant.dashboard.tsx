8e1237ef066d52dfb290f8e279f7741f5af36f38
ppwlsw <pinpawat.l@ku.th>
2025-02-19 23:27:40 +0700

    add:reservation card component

--- app/routes/_merchant-sidebar.merchant.dashboard.tsx
+++ app/routes/_merchant-sidebar.merchant.dashboard.tsx
@@ -1,5 +1,11 @@
+import ReservationCard from "../components/reservation-card";
+
 function DashboardPage() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <ReservationCard></ReservationCard>
+    </div>
+  );
 }
 
 export default DashboardPage;
