c3ae00021d0e857651b9c807066da8ab1d134119
B1gdawg0 <bkaosas@gmail.com>
2025-03-23 21:57:53 +0700

    update: update user image profile

--- app/repositories/user.repository.ts
+++ app/repositories/user.repository.ts
@@ -1,5 +1,8 @@
 import type { User } from "~/types/user";
 import useAxiosInstance from "~/utils/axiosInstance";
+import fs from "fs";
+import axios from "axios";
+import { useAuth } from "~/utils/auth";
 
 const API_BASE_URL: string = process.env.API_BASE_URL as string;
 
@@ -22,7 +25,40 @@ export async function defaultFetcherUserInfo(request:Request, user_id: number, t
   return user
 }
 
+export async function updateUserAvatar(request: Request, user_id: number, file: File) {
+    try {
+      const { getCookie } = useAuth;
+      const cookie = await getCookie({ request });
+      const token = cookie?.token;
+      if (!token) throw Error("Unauthorized");
 
+      const formData = new FormData();
+      formData.append("image", file);
+
+      const response = await fetch(
+        `${process.env.API_BASE_URL}/users/${user_id}/avatar`, 
+        {
+          method: 'POST',
+          headers: {
+            "Authorization": `Bearer ${token}`
+          },
+          body: formData
+        }
+      );
+      
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error("Error response:", errorData);
+        throw new Error(errorData.message || "Failed to upload image");
+      }
+      
+      const responseData = await response.json();
+      return responseData.data;
+    } catch (e) {
+      console.error("Upload error:", e);
+      throw e;
+    }
+}
 
 export async function updateUserInfo(request:Request, user_id:number, {name, phone}:{name:string; phone:string}) {
     try{
